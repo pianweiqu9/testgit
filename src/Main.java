9416e35f02bc40a13bdb41d66781d4f8fc7c04eb
renr <123444@aliyun.com>
2018-10-14 17:28:21 +0800

    新增代码内容后

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("更新了");
+        System.out.println("第一行");
         System.out.println("又一行信息");
     }
 }
